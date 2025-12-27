e0900ab4b11f631171e26846ac697d7b2e327a75
ShashEatsBeans <shashwat.avin@gmail.com>
2025-12-27 00:26:34 -0500

    Adding back port

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const path = require('path');
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   process.env.FRONTEND_URL
